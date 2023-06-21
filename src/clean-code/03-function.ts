309f49e3b937fbb23d00b3101806aca2fbe1af5e
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2023-06-20 20:52:04 -0600

    feat: unidad 2

--- src/clean-code/03-function.ts
+++ src/clean-code/03-function.ts
@@ -29,11 +29,21 @@
   // Crea un nuevo actor
   function createActor(fullName: string, birthdate: Date): boolean {
     // tarea asincrona para verificar nombre
-    // ..
-    // ..
     if (fullName === "fernando") return false;
 
     console.log("Crear actor");
     return true;
   }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
 })();
